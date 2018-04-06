76739f320478e7048aa224d932e3074ebd3d99a7
Marcel <marcel.k@just-click.org>
2018-04-06 11:24:31 +0200

    Update config.js

--- node-server/config/config.js
+++ node-server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "database": "postgres://postgres:postgres@localhost:5432/ngtime",
     "secret": "ngTime", // Only for Test
-    "trelloKey": "bc9e431dedeef45bf29170c794a0dec3",
-    "trelloToken": "57592e927e15a25c88382e35472240d5081ee89550eb47165244ee8ab03d27f0"
+    "trelloKey": "",
+    "trelloToken": ""
 };
