08bb2aa3a06f6f02205d166d324c2f2c8207546f
Marcel <marcel.k@just-click.org>
2018-06-20 22:59:25 +0200

    Refactoring of ngTime-Rest

--- ngTime-rest/config/config.js
+++ ngTime-rest/config/config.js
@@ -1,6 +1,4 @@
 module.exports = {
     "database": "postgres://postgres:postgres@localhost:5432/ngtime",
     "secret": "ngTime", // Only for Test
-    "trelloKey": "",
-    "trelloToken": ""
 };
--- ngTime-rest/routes/index.js
+++ ngTime-rest/routes/index.js
@@ -2,18 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var userq = require('./api/user_route');
-var timerecords = require('./api/timerecord_route');
 var tasks = require('./api/task_route');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/api/timerecord', timerecords.getAllTimeRecords);
-router.get('/api/timerecord/:id', timerecords.getSingleTimeRecord);
-router.post('/api/timerecord', timerecords.createTimeRecord);
-router.delete('/api/timerecord/:id', timerecords.removeTimeRecord);
 
 router.get('/api/tasks', tasks.getAllTasks);
 router.post('/api/tasks', tasks.createNewTask);
